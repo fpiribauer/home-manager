c02763ae04a4638a2773b3e5b07c70d877d7d546
Fabian Piribauer <fabian.piribauer@gmail.com>
2024-02-06 18:12:43 +0100

    add comments

--- neovim.nix
+++ neovim.nix
@@ -39,52 +39,6 @@
         '';
       }
       {
-        plugin = null-ls-nvim;
-        type = "lua";
-        config = ''
-          local null_ls = require("null-ls")
-          local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-          null_ls.setup({
-              debug = true,
-              sources = {
-                  null_ls.builtins.formatting.alejandra.with({
-                      command = "${pkgs.alejandra}/bin/alejandra"
-                  }),
-                  null_ls.builtins.formatting.lua_format.with({
-                      command = "${pkgs.luaformatter}/bin/lua-format"
-                  }),
-                  null_ls.builtins.formatting.google_java_format.with({
-                      command = "${pkgs.google-java-format}/bin/google-java-format"
-                  }),
-                  null_ls.builtins.formatting.clang_format.with({
-                      command = "${pkgs.clang-tools}/bin/clang-format"
-                  }),
-                  null_ls.builtins.formatting.black.with({
-                      command = "${pkgs.black}/bin/black"
-                  })
-              },
-
-              on_attach = function(client, bufnr)
-                  if client.supports_method("textDocument/formatting") then
-                      vim.api.nvim_clear_autocmds({group = augroup, buffer = bufnr})
-                      vim.api.nvim_create_autocmd("BufWritePre", {
-                          group = augroup,
-                          buffer = bufnr,
-                          callback = function()
-                              vim.lsp.buf.format({
-                                  bufnr = bufnr,
-                                  filter = function(client)
-                                      return client.name == "null-ls"
-                                  end
-                              })
-                          end
-                      })
-                  end
-              end
-          })
-        '';
-      }
-      {
         plugin = bufferline-nvim;
         type = "lua";
         config = ''
@@ -221,6 +175,13 @@
             },
           })
           vim.o.updatetime = 250
+        '';
+      }
+      {
+        plugin = comment-nvim;
+        type = "lua";
+        config = ''
+          require('Comment').setup()
         '';
       }
     ];
