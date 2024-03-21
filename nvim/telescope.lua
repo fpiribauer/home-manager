3f6ba9054e99e1f1fe03a744cc3defd34f841d8a
Fabian Piribauer <fabian.piribauer@gmail.com>
2024-03-21 16:13:51 +0100

    add nvim commands for searching hidden files

--- nvim/telescope.lua
+++ nvim/telescope.lua
@@ -3,4 +3,6 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>af', function() builtin.find_files({ hidden = true, follow = true }) end, {})
+vim.keymap.set('n', '<leader>ag', function() builtin.live_grep({ additional_args = { "--hidden" } }) end, {})
 require("telescope").setup{}
