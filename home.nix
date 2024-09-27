631214c5011b5918b0c09c34de1a9e1b09674322
Fabian Piribauer <fabian.piribauer@gmail.com>
2024-09-27 21:57:28 +0200

    add code formatter

--- home.nix
+++ home.nix
@@ -1,20 +1,19 @@
 {
   pkgs,
-  unstablePkgs,
-  config,
   dotfiles,
   nix-colors,
   nix-index-database,
   ...
-}@inputs: 
+}@inputs:
 let
-  mylib = import ./mylib inputs; 
-in {
+  mylib = import ./mylib inputs;
+in
+{
   imports = [
     nix-colors.homeManagerModules.default
     nix-index-database.hmModules.nix-index
     ./nvim/neovim.nix
-#    ./wayland/sway.nix honestly its a mess, so we just use system way with its config...
+    #    ./wayland/sway.nix honestly its a mess, so we just use system way with its config...
     ./wayland
     ./wasm
     ./esp32/esp32.nix
@@ -26,48 +25,57 @@ in {
 
   home.sessionPath = [
     "/opt/intelFPGA/20.1/modelsim_ase/bin"
-     "$HOME/.pyenv/bin"
+    "$HOME/.pyenv/bin"
   ];
   home.sessionVariables = {
-    _JAVA_AWT_WM_NONREPARENTING=1; # Makes Java GUIs work nice with sway
+    _JAVA_AWT_WM_NONREPARENTING = 1; # Makes Java GUIs work nice with sway
   };
 
   #colorScheme = nix-colors.colorSchemes.base24.dracula;
   colorScheme = nix-colors.colorSchemes.base16.gruvbox-material-dark-medium;
   fonts.fontconfig.enable = true;
-  home.packages = (with pkgs; [
-    # Audio
-    pipewire
-    wireplumber
-    # User Program
-    anki
-    # pyenv -- time is not yet ripe enough for such powerful inventions
-    (
-      nerdfonts.override
-      {
-        fonts = ["FiraCode" "JetBrainsMono" "Iosevka" "CascadiaCode" "CascadiaMono"];
-      }
-    )
-    # LSP Stuff
-    nixd
-    ruff-lsp
-    pyright
-    pavucontrol
-    rust-analyzer
-    tmux-sessionizer
-    clang-tools_17
-    vscode
-    vscode-extensions.ms-vscode.cpptools
-    # Devops
-    ansible
-    ansible-lint
-    # Mail
-    thunderbird
-  ]);
+  home.packages = (
+    with pkgs;
+    [
+      # Audio
+      pipewire
+      wireplumber
+      # User Program
+      anki
+      # pyenv -- time is not yet ripe enough for such powerful inventions
+      (nerdfonts.override {
+        fonts = [
+          "FiraCode"
+          "JetBrainsMono"
+          "Iosevka"
+          "CascadiaCode"
+          "CascadiaMono"
+        ];
+      })
+      # LSP Stuff
+      nixd
+      nixfmt-rfc-style
+      ruff-lsp
+      pyright
+      pavucontrol
+      rust-analyzer
+      tmux-sessionizer
+      clang-tools_17
+      vscode
+      vscode-extensions.ms-vscode.cpptools
+      # Devops
+      ansible
+      ansible-lint
+      # Mail
+      thunderbird
+    ]
+  );
 
   programs.home-manager.enable = true;
   programs.bash = {
     enable = true;
-    initExtra = builtins.readFile (mylib.utils.renderTemplate { template ="${dotfiles}/bash/bashrc"; });
+    initExtra = builtins.readFile (
+      mylib.utils.renderTemplate { template = "${dotfiles}/bash/bashrc"; }
+    );
   };
 }
