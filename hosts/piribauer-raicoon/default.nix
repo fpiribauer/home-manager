072beefab71b18089abe1d6f5bb54e1717d97dc1
Fabian Piribauer <f.piribauer@raicoon.com>
2025-02-06 08:06:45 +0100

    Add bump2version for work laptop

--- hosts/piribauer-raicoon/default.nix
+++ hosts/piribauer-raicoon/default.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   cst.wayland.enable = true;
 
   home.username = "piribauer";
   home.homeDirectory = "/home/piribauer";
+  home.packages = with pkgs; [
+    bump2version
+  ];
+
 }
