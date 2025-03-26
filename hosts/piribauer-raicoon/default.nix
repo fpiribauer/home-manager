8e2fe72babee92c7fd3f5913eef9c640ca5ddcb5
Fabian Piribauer <f.piribauer@raicoon.com>
2025-03-26 07:00:35 +0100

    update flake

--- hosts/piribauer-raicoon/default.nix
+++ hosts/piribauer-raicoon/default.nix
@@ -18,6 +18,9 @@
     path = "/home/piribauer/raicoon"
     depth = 3
     [[search_dirs]]
+    path = "/home/piribauer/my_demos"
+    depth = 1
+    [[search_dirs]]
     path = "/home/piribauer/.config/home-manager"
     depth = 1
     '';
