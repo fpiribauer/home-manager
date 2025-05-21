bd93d18079474f41f1a8cc60c56614cd233bfa1a
Fabian Piribauer <f.piribauer@raicoon.com>
2025-05-21 16:22:11 +0200

    minor improvements

--- nvim/config.lua
+++ nvim/config.lua
@@ -3,6 +3,7 @@ vim.api.nvim_set_option_value("colorcolumn", "79", {})
 vim.api.nvim_set_option_value("signcolumn", "yes:1", {})
 
 vim.g.mapleader = " "
+vim.o.timeout = false
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
--- nvim/telescope.lua
+++ nvim/telescope.lua
@@ -5,4 +5,7 @@ vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>af', function() builtin.find_files({ hidden = true, follow = true }) end, {})
 vim.keymap.set('n', '<leader>ag', function() builtin.live_grep({ additional_args = { "--hidden" } }) end, {})
+vim.keymap.set('n', '<leader>gr', builtin.lsp_references, {})
+vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
+
 require("telescope").setup{}
